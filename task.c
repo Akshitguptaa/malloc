a35764968a06225cdb2025a6fb856cc93fdc0851
akshitguptaa <akshitguptaa29@gmail.com>
2024-12-24 00:29:21 +0530

    added tasks

---
+++ task.c
@@ -0,0 +1,9 @@
+/*
+    add->
+    Detecting memory leaks 
+    memory pool or slab allocator for faster allocations of fixed-size blocks
+    Using a best-fit, worst-fit, or first-fit algorithm for block allocation
+    add thread-safety
+
+    implementing a garbage-collector
+*/ 
